abe58f11c2ba50e10c63b8107603852c80cca61a
Ivan Mukoed <1rydler@gmail.com>
2022-01-04 20:22:36 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import Game from './components/Game';
 
 function App() {
+  const [fetchedData, setFetchedData] = useState({})
+  const [mode, setMode] = useState("")
+  const [playing, setPlaying] = useState(false)
+
+  const fetchData = async () => {
+    const res = await fetch('https://demo1030918.mockable.io/')
+    const data = await res.json()
+    setFetchedData(data)
+  }
+
+  const startGame = () => {
+    if (mode !== "") {
+      setPlaying(true)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App p-5">
+      <div>
+        <h2 className='font-bold py-3 text-3xl'>Please, select a mode and click "Start"! </h2>
+        <div className='flex'>
+          <select className='border border-black p-2' value={mode} onChange={(e) => setMode(e.target.value)}>
+            <option value="" disabled>Select game mode</option>
+            {Object.keys(fetchedData).map((mode) => (
+              <option key={mode} value={mode}>{mode}</option>
+            ))}
+          </select>
+          <button className='bg-sky-500 uppercase text-white rounded-md px-3 py-1 mx-2 font-bold' onClick={() => startGame()}>Start</button>
+        </div >
+      </div>
+      {playing ? <Game fields={fetchedData[mode].field} /> : ""}
+
+    </div >
   );
 }
 
---
+++ src/components/Game.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react'
+import Square from './Square';
+
+const Game = ({ fields }) => {
+    const [indexes, setIndexes] = useState([])
+    const [alerts, setAlerts] = useState([])
+
+    const mapIndexes = () => {
+        let newArr = []
+        for (let i = 1; i < fields + 1; i++) {
+            newArr.push(i)
+        }
+        setIndexes(newArr)
+    }
+
+    const handleHover = (row_id, square_id) => {
+        let newArr = [{ row_id: row_id, square_id: square_id }, ...alerts]
+        // check for alerts array length and cutting it so it doesn't become too big for screen
+        if (newArr.length > 5) {
+            newArr = newArr.splice(0, 5)
+        }
+        setAlerts(newArr)
+    }
+
+    useEffect(() => {
+        mapIndexes()
+    }, [fields])
+
+    return (
+        <div className='flex flex-wrap p-5'>
+            <div>
+                {indexes.map((row_id, idx) => (
+                    <div key={idx} className="flex-nowrap flex">
+                        {indexes.map((square_id, idx) => (
+                            <Square key={idx} square_id={square_id} row_id={row_id} handleHover={handleHover} />
+                        ))}
+                    </div>
+                ))}
+            </div>
+            <div className='pl-5'>
+                <div className='font-bold py-3 text-3xl'>Hover Squares</div>
+                <div className='alerts'>
+                    {alerts && alerts.length > 0 ? alerts.map((alert, idx) => (
+                        <div key={idx} className="bg-amber-300 p-3 my-2 font-bold rounded-md">row {alert.row_id} square {alert.square_id}</div>
+                    )) : ""}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Game
