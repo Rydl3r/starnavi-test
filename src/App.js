ec17dd22fd71d4154cb3fc6ee01734cb79f56cc6
Ivan Mukoed <1rydler@gmail.com>
2022-06-14 17:32:13 +0300

    syntax fixes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,58 @@
-import { useEffect, useState } from 'react'
-import Game from './components/Game';
+import { useEffect, useState } from "react";
+import Game from "./components/Game";
 
 function App() {
-  const [fetchedData, setFetchedData] = useState({})
-  const [mode, setMode] = useState("")
-  const [playing, setPlaying] = useState(false)
+  const [fetchedData, setFetchedData] = useState({});
+  const [mode, setMode] = useState("");
+  const [playing, setPlaying] = useState(false);
 
   const fetchData = async () => {
-    const res = await fetch('https://demo1030918.mockable.io/')
-    const data = await res.json()
-    setFetchedData(data)
-  }
+    const res = await fetch("https://demo1030918.mockable.io/");
+    const data = await res.json();
+    setFetchedData(data);
+  };
 
   const startGame = () => {
     if (mode !== "") {
-      setPlaying(true)
+      setPlaying(true);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   return (
     <div className="App p-5">
       <div>
-        <h2 className='font-bold py-3 text-3xl'>Please, select a mode and click "Start"! </h2>
-        <div className='flex'>
-          <select className='border border-black p-2' value={mode} onChange={(e) => setMode(e.target.value)}>
-            <option value="" disabled>Select game mode</option>
+        <h2 className="font-bold py-3 text-3xl">
+          Please, select a mode and click "Start"!{" "}
+        </h2>
+        <div className="flex">
+          <select
+            className="border border-black p-2"
+            value={mode}
+            onChange={(e) => setMode(e.target.value)}
+          >
+            <option value="" disabled>
+              Select game mode
+            </option>
             {Object.keys(fetchedData).map((mode) => (
-              <option key={mode} value={mode}>{mode}</option>
+              <option key={mode} value={mode}>
+                {mode}
+              </option>
             ))}
           </select>
-          <button className='bg-sky-500 uppercase text-white rounded-md px-3 py-1 mx-2 font-bold' onClick={() => startGame()}>Start</button>
-        </div >
+          <button
+            className="bg-sky-500 uppercase text-white rounded-md px-3 py-1 mx-2 font-bold"
+            onClick={() => startGame()}
+          >
+            Start
+          </button>
+        </div>
       </div>
       {playing ? <Game fields={fetchedData[mode].field} /> : ""}
-
-    </div >
+    </div>
   );
 }
 
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Square = ({ square_id, row_id, handleHover }) => {
-    const [hovered, setHovered] = useState(false)
-    return (
-        <div key={square_id} onMouseEnter={() => {
-            setHovered(!hovered)
-            handleHover(row_id, square_id)
-        }} className={`square p-5 border border-black cursor-pointer ${hovered ? 'bg-sky-500' : ''}`}></div>
-    )
-}
+  const [hovered, setHovered] = useState(false);
+  return (
+    <div
+      key={square_id}
+      onMouseEnter={() => {
+        setHovered(!hovered);
+        handleHover(row_id, square_id);
+      }}
+      className={`square p-5 border border-black cursor-pointer ${
+        hovered ? "bg-sky-500" : ""
+      }`}
+    ></div>
+  );
+};
 
-export default Square
+export default Square;
